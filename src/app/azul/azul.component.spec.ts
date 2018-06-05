0311ad399936ab28a2832e5318ba67df76641347
Ryan <Ryan@CROCODILE>
2018-06-05 09:31:12 -0400

    Start testing

--- src/app/azul/azul.component.spec.ts
+++ src/app/azul/azul.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AzulComponent } from './azul.component';
+import { AzulTile } from '../classes/azul-tile';
 
 describe('AzulComponent', () => {
   let component: AzulComponent;
@@ -17,9 +18,38 @@ describe('AzulComponent', () => {
     fixture = TestBed.createComponent(AzulComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('score 0 init', () => {
+    let t =true;
+    expect(t).toBeTruthy();
+  });
+
+  it('toggleTileSet toggles hasTile TRUE', () => {
+    let b = component.game.TileBoard;
+    let g = component.game;  
+    component.game.toggleTileSet(0);
+    let t = b[0].hasTile;
+     
+    expect(t).toBeTruthy();
+  });
+
+  it('toggleTileSet toggles hasTile FALSE', () => {
+    let b = component.game.TileBoard;
+    let g = component.game; 
+    let t = b[0].hasTile; 
+    component.game.toggleTileSet(0);
+
+    expect(t).toBeFalsy();
+  });
+
+
+
+
+
 });
